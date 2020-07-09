a92881b3d6cce93856364ab765019d73428ed107
Om Dhanwant <>
2020-07-09 22:46:01 +0530

    -- initial commit

---
+++ app/src/main/java/com/omdhanwant/youstream/utils/Constants.java
@@ -0,0 +1,9 @@
+package com.omdhanwant.youstream.utils;
+
+public class Constants {
+
+    public static final String BASE_URL = "https://api.themoviedb.org/3/";
+    public static final String IMAGE_BASE_URL = "http://image.tmdb.org/t/p/w185/";
+    public final static String API_KEY = "c1efc8d503483eb9469d7ead69e1a683";
+    public static final String DEVELOPER_KEY = "AIzaSyCK4Yqf6gOrTnMeMO-9xytSk4RvSyGznBg";
+}
